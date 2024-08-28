a58dcdadea4bb4099f1f36e8ac3a4580465fe22d
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2024-08-28 19:32:57 +0200

    Add Poppins font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,30 @@
+import Header from "@/components/Header";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+
+  useFonts({
+    "Poppins": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-med": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-extraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+  });
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack 
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen 
+        name="index" 
+        options={{
+          headerShown: true,
+          header: () => <Header />
+        }}
+      />
     </Stack>
   );
 }
